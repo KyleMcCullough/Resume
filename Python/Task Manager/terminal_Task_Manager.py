16b8dae1f9d785fca00d70f4b3f6ca333b42c2c6
KyleMcCullough <mcculloughkyle26@gmail.com>
2020-02-24 12:50:50 -0400

    Added get category method, new task method.
    BUGS:
    Enter new task task does not display categories
    Enter 1-3 section takes other integers

--- Python/Task Manager/terminal_Task_Manager.py
+++ Python/Task Manager/terminal_Task_Manager.py
@@ -1,27 +1,55 @@
 
 
-def enterNewTask():
-    pass
+def enterNewTask(list):
+    task = ""
+    category = ""
+    while True:
+        task = input("Enter the new task:")
+
+        if task.isalpha():
+            break
+    
+    while True:
+        print("The categories are:\n")
+        for i in range(len(list[1])):
+            print(f"{list[1][i]}")
+
+        category = input("Enter the category:")
+
+        if task.isalpha():
+            break
+    print("YYYYYYY")
 
 def getCategory(id):
-    
     category = ["Food", "Chores", "Exercise", "Work", "Other"]
     for i in range(5):
-        if list[1][i] == i:
+        if id == i:
             return category[i]
 
 def displayList(list):
+    print("------------------------------------------\nTask\t\t\t\tCategory\n")
     for i in range(len(list[0])):
-
-
-        print(f"{list[0][i]}\t\t\t{getCategory(list[1][i])}", end="")
+        print(f"{list[0][i]}\t\t\t{getCategory(list[1][i])}")
 
 def main():
-
-    
     tasksList = [["Eat Breakfast", "Do homework", "Clean room", "Go to gym"], [0,3,1,2]]
-    
-    userInput = 0
     displayList(tasksList)
+
+    userInput = 0
+    
+    print("Press 1 to enter a new task, 2 to delete a task or 3 to refresh the task list.")
+    
+    while True:
+    
+        userInput = input("Enter a value from 1-3: ")
+
+        try: 
+            int(userInput) not in range(1,3)
+            break
+        except:
+            continue
+    
+    if userInput == "1":
+        tasksList = enterNewTask(tasksList)
 
 main()
