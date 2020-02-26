f9e20752efcbc1bd4f782221c6b697f76ada253b
KyleMcCullough <mcculloughkyle26@gmail.com>
2020-02-26 10:43:49 -0400

    Fixes: Fixed formatting Added categoryList to tasksList
    Issues:
    enterNewTasks method:
    Category is not properly checked
    Spacing is inconsistent

--- Python/Task Manager/terminal_Task_Manager.py
+++ Python/Task Manager/terminal_Task_Manager.py
@@ -4,24 +4,23 @@ def enterNewTask(list):
     task = ""
     category = ""
     while True:
-        task = input("Enter the new task:")
+        task = input("Enter the new task: ")
 
         if task.isalpha():
             break
     
     while True:
-        print("The categories are:\n")
-        for i in range(len(list[1])):
-            print(f"{list[1][i]}")
+        print("The categories are: ", end="")
+        for i in range(len(list[2])):
+            print(f"{list[2][i].capitalize()}", end=", ")
 
-        category = input("Enter the category:")
+        category = input("\n\nEnter the category:")
 
-        if task.isalpha():
-            break
+        if category.lower in list[2]:
+                break
     print("YYYYYYY")
 
-def getCategory(id):
-    category = ["Food", "Chores", "Exercise", "Work", "Other"]
+def getCategory(id, category):
     for i in range(5):
         if id == i:
             return category[i]
@@ -29,15 +28,15 @@ def getCategory(id):
 def displayList(list):
     print("------------------------------------------\nTask\t\t\t\tCategory\n")
     for i in range(len(list[0])):
-        print(f"{list[0][i]}\t\t\t{getCategory(list[1][i])}")
+        print(f"{list[0][i]}\t\t\t{getCategory(list[1][i], list[2])}")
 
 def main():
-    tasksList = [["Eat Breakfast", "Do homework", "Clean room", "Go to gym"], [0,3,1,2]]
+    tasksList = [["Eat Breakfast", "Do homework", "Clean room", "Go to gym"], [0,3,1,2], ["food", "chores", "exercise", "work", "other"]]
     displayList(tasksList)
 
     userInput = 0
     
-    print("Press 1 to enter a new task, 2 to delete a task or 3 to refresh the task list.")
+    print("\nPress 1 to enter a new task, 2 to delete a task or 3 to refresh the task list.")
     
     while True:
     
